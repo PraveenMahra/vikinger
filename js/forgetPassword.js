0a53453ac0969d40e381f5b1ca50822ae059f95a
PraveenMahra <praveen.mehra65@gmail.com>
2024-03-01 12:59:11 +0530

    add some validation

--- js/forgetPassword.js
+++ js/forgetPassword.js
@@ -4,9 +4,11 @@ const btn = document.querySelector(".new-pass-btn");
 btn.addEventListener("click", function (e) {
   e.preventDefault();
 
+  if (!email.value) {
+    alert("enter your email address");
+  }
   if (email.value) {
     alert("password send to your email");
+    window.location.href = "/";
   }
-
-  window.location.href = "/";
 });
--- js/login.js
+++ js/login.js
@@ -5,9 +5,13 @@ const loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
+  if (!email.value) {
+    alert("please enter your email address");
+  } else if (!password.value) {
+    alert("please enter your password");
+  }
   if (email.value && password.value) {
     alert("login successfully");
+    window.location.href = "/";
   }
-
-  window.location.href = "/";
 });
